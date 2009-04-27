0abdef0cbcce4c54d85d93f44386e15c376bc673
M. Rohrmoser <work@mro.name>
2009-04-27 12:12:04 +0200

    initial hampel

---
+++ demos/hampelmann/src/main/java/mobi/alsw/jugm/hampel/Driver.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package mobi.alsw.jugm.hampel;
+
+import javax.swing.JFrame;
+
+/**
+ * @author m
+ *
+ */
+public class Driver extends JFrame {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
